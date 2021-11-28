9a2049b57cbe13a996cdb986cdac47258ddd1dff
EC2 Default User <ec2-user@ip-172-31-95-24.ec2.internal>
2021-11-28 05:43:08 +0000

    [Finish] 応用課題フェーズ課題２

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books
+
+  root 'homes#top'
+  get 'home/about' => 'homes#about'
+
+end
