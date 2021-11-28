36e37f5a23356ba7a79e371e9fb3b37a344bffa3
EC2 Default User <ec2-user@ip-172-31-95-24.ec2.internal>
2021-11-28 08:19:37 +0000

    [Finish] 応用課題フェーズ課題３

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,30 @@
+class BookCommentsController < ApplicationController
+
+  def create
+    @book = Book.find(params[:book_id])
+    book_comment = @book.book_comments.new(book_comment_params)
+    book_comment.user_id = current_user.id
+    book_comment.book_id = @book.id
+    if book_comment.save
+      redirect_to book_path(@book)
+    else
+      render 'books/show'
+    end
+
+  end
+
+  def destroy
+    @book = Book.find(params[:book_id])
+    book_comment = @book.book_comments.find_by(params[:id])
+    book_comment.destroy
+    redirect_to book_path(@book)
+
+  end
+
+  private
+
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
 
   root 'homes#top'
   get 'home/about' => 'homes#about'
